6656ab2fcbbe575065c3ae9ad2b04fae82510a8c
Ben Cawrse <bcawrse@gmail.com>
2022-01-03 14:44:41 -0600

    Module 2 changes

---
+++ sick-fits/frontend/components/Page.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+
+import Header from './Header';
+
+export default function Page({ children, cool }) {
+  return (
+    <div>
+      <Header />
+      <h2>I am the page component</h2>
+      <hd>{cool}</hd>
+      {children}
+    </div>
+  );
+}
+
+Page.propTypes = {
+  cool: PropTypes.string,
+  children: PropTypes.any,
+};
